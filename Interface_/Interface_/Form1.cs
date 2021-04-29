57c1437f9300c8ceaf73128ced40d8620f07b5e6
Sadigzade Mikayil <75858136+mikayil1551@users.noreply.github.com>
2021-04-29 13:51:02 +0300

    Add files via upload

---
+++ Interface_/Interface_/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interface_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Oyuncu o = new Oyuncu();
+            o.Adi = "Mikayil";
+            o.Yasi = 22;
+            o.YasamDegeri = 200;
+            o.Silahi = new Kanas();
+            MessageBox.Show(o.NisanAl());
+            Oyuncu o2 = new Oyuncu();
+            Bicak bck = new Bicak();
+            o2.Silahi = bck;
+            MessageBox.Show(o2.NisanAl());
+        }
+    }
+}
---
+++ Interface_/Interface_/KesiciSilahlar.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_
+{
+    class KesiciSilahlar:Silah
+    {
+        public float Dayaniklilik{ get; set; }
+
+    }
+}
